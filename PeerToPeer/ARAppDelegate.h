fb00c361a719a3b878e01334fb2f7f7c458f88cf
Huy Le <lmnhuy@gmail.com>
2014-04-01 10:22:45 +0700

    Init project

---
+++ PeerToPeer/ARAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ARAppDelegate.h
+//  PeerToPeer
+//
+//  Created by admin on 4/1/14.
+//  Copyright (c) 2014 Huy Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PeerToPeer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PeerToPeer
+//
+//  Created by admin on 4/1/14.
+//  Copyright (c) 2014 Huy Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ARAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ARAppDelegate class]));
+    }
+}
